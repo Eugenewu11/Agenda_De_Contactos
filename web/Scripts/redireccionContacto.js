9328ced923d835aaa5c08ac42dee69a7cdb0d088
Eugenewu11 <genewu013@gmail.com>
2025-07-06 14:51:34 -0600

    13-Cambios en frontend

--- web/Scripts/redireccionContacto.js
+++ web/Scripts/redireccionContacto.js
@@ -2,6 +2,17 @@
 document.addEventListener('DOMContentLoaded', () => {
   const contactos = document.querySelectorAll('.contact-container');
 
+  // Manejo del clic en call-btn (debe evitar propagación)
+  const callButtons = document.querySelectorAll('.call-btn');
+  callButtons.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+      e.stopPropagation(); 
+      console.log('Llamando al contacto...');
+      
+    });
+  });
+
+  // Clic en el contenedor para abrir perfil
   contactos.forEach((contacto) => {
     contacto.addEventListener('click', () => {
       const nombre = contacto.querySelector('.contact-name').textContent.trim();
@@ -12,5 +23,9 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 //! Abre nueva pagina para guardar nuevo contacto
+document.getElementById("aggContacto").addEventListener("click", function () {
+    window.location.href = "crearContacto.html";
+  });
+
 
 //TODO: Que la redireccion sea segun el id de contacto.
